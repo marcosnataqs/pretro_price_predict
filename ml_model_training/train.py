b553b399a8ec69b0abd069fcc69d5994dc7afe5c
Júlio Bernardes <juliocd.bernardes@gmail.com>
2024-10-20 10:43:03 -0300

    Adjusting train.py to use utils

--- ml_model_training/train.py
+++ ml_model_training/train.py
@@ -12,67 +12,10 @@ from petro_dataset import PetroDataset
 from petro_model import PetroModel
 from sklearn.model_selection import train_test_split
 
+##Testing Utils
+from model_utils import train_one_epoch, validate_one_epoch
+
 load_dotenv()
-
-
-def train_one_epoch(
-    epoch: int,
-    train_loader: DataLoader,
-    model: PetroModel,
-    loss_function: nn.MSELoss,
-    optimizer: optim.Adam,
-    device: str,
-) -> None:
-    print(f"Training Epoch: {epoch+1}")
-    model.train(True)
-    running_loss = 0.0
-
-    for batch_index, batch in enumerate(train_loader):
-        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
-
-        outputs = model(x_batch)
-        loss = loss_function(outputs, y_batch)
-        running_loss += loss.item()
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch_index % 100 == 99:
-            avg_loss = running_loss / 100
-            print(f"Batch {batch_index + 1}, Loss: {avg_loss}")
-            neptune_run["train/batch_loss"].log(avg_loss)
-            running_loss = 0.0
-
-    neptune_run["train/epoch"].log(epoch + 1)
-    print()
-
-
-# TO-DO: We need to review this part if we are going to test with batches or always the same data
-def validate_one_epoch(
-    epoch: int,
-    model: PetroModel,
-    test_loader: DataLoader,
-    device: str,
-    loss_function: nn.MSELoss,
-) -> None:
-    print(f"Validation Epoch: {epoch+1}")
-    model.train(False)
-    running_loss = 0.0
-
-    for batch_index, batch in enumerate(test_loader):
-        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
-
-        with torch.no_grad():
-            output = model(x_batch)
-            loss = loss_function(output, y_batch)
-            running_loss += loss.item()
-
-    avg_loss_across_batches = running_loss / len(test_loader)
-    print(f"Validation Loss: {avg_loss_across_batches}")
-    neptune_run["validation/loss"].log(avg_loss_across_batches)
-    print("*********************************************")
-    print()
-
 
 def main(
     num_epochs: int,
