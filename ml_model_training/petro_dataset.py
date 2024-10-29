a1c5571bb3dbda35eaf5c54576e0ca2c90575fb8
Júlio Bernardes <juliocd.bernardes@gmail.com>
2024-10-29 20:19:24 -0300

    (petro_dataset.py) - fixing petro dataset to separate X from y before scale and then generate separate scalers for server

--- ml_model_training/petro_dataset.py
+++ ml_model_training/petro_dataset.py
@@ -28,43 +28,54 @@ class PetroDataset(Dataset):
         This is a custom method where will be implemented data transformations,
         the idea is to simply pass the crude data to the class and PetroDataset will take care od the test.
         """
-        self.add_lags()
+        # self.add_lags()
+        self.split_data()
         self.scale_data()
         self.reshape_data()
 
-    def add_lags(self) -> None:
-        """
-        This method will use the pipeline parameters to generate the lags for the columns I chose.
-        This means that I can interate each every column I want and set a number or lags that I may use in my model later.
-        """
-        for column in self.pipeline_params["columns"]:
-            for i in range(1, self.pipeline_params["num_lags"] + 1):
-                self.data[f"{column}_(t-{i})"] = self.data[column].shift(i)
-            self.num_features += self.pipeline_params["num_lags"]
-        self.data.dropna(inplace=True)
-        # self.data.drop(columns=['adj_pbr','brent','wti','production','usd'])
-        # self.num_features -= 4
+    # def add_lags(self) -> None:
+    #     """
+    #     This method will use the pipeline parameters to generate the lags for the columns I chose.
+    #     This means that I can interate each every column I want and set a number or lags that I may use in my model later.
+    #     """
+    #     for column in self.pipeline_params["columns"]:
+    #         for i in range(1, self.pipeline_params["num_lags"] + 1):
+    #             self.data[f"{column}_(t-{i})"] = self.data[column].shift(i)
+    #         self.num_features += self.pipeline_params["num_lags"]
+    #     self.data.dropna(inplace=True)
+    #     # self.data.drop(columns=['adj_pbr','brent','wti','production','usd'])
+    #     # self.num_features -= 4
 
+    def split_data(self) -> None:
+        self.input_data = self.data.drop(columns='target', axis=1)
+        self.output_data = self.data['target']
+    
     def scale_data(self) -> None:
         """
         This method is used to scale my train data in the pipeline,
         it also saves my scaler as a joblib/pickle so it can be used later.
         """
-        scaler_filename = os.path.join("ml_model_training", "scaler_petro.joblib")
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        self.data_scaled = scaler.fit_transform(self.data)
-        dump(scaler, scaler_filename)
+        input_scaler_filename = os.path.join("ml_model_training", "input_scaler_petro.joblib")
+        output_scaler_filename = os.path.join("ml_model_training", "output_scaler_petro.joblib")
+        
+        input_scaler = MinMaxScaler(feature_range=(-1, 1))
+        output_scaler = MinMaxScaler(feature_range=(-1, 1))
+
+        self.input_data = input_scaler.fit_transform(self.input_data)
+        self.output_data = output_scaler.fit_transform(self.output_data.values.reshape(-1,1))
+
+        # self.data_scaled = scaler.fit_transform(self.data)
+        dump(input_scaler, input_scaler_filename)
+        dump(input_scaler, output_scaler_filename)
 
     def reshape_data(self) -> None:
         """
         Ths method will reshape my data spliting the target from the data.
         It also adjusts the dimensions of the numpy array so it matches the required dimensions for LSTM
         """
-        X = np.flip(self.data_scaled[:, 1:], axis=1)
-        print(X)
-        y = self.data_scaled[:, 0]
-        print(self.num_features)
-        self.X = X.reshape((-1, (self.num_features-1), 1))
+        X = np.flip(self.input_data, axis=1)
+        y = self.output_data
+        self.X = X.reshape((-1, (self.num_features), 1))
         self.y = y.reshape((-1, 1))
         print(self.X.shape, self.y.shape)
         print(type(self.X), type(self.y))
@@ -89,10 +100,12 @@ class PetroDataset(Dataset):
 
 
 if __name__ == "__main__":
+    from data_engineering.data_utils import add_lags
     data = pd.read_csv(
         os.path.join("ml_model_training", "petro_2.csv"), index_col=0, parse_dates=True
     ).sort_index()
-    params = {"num_lags": 7, "columns": ["pbr"], "num_features": 1}
+    data = add_lags(data, 7, columns=['target'])
+    params = {"num_features": 7}
     petrodata = PetroDataset(data, pipeline_params=params)
     data, target = petrodata[1]
     print(data, target)
