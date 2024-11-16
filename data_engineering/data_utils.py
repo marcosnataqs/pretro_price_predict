ac15856062200604a4088eebe24ccc224a86b7d5
Brisa <brisamnascimento@gmail.com>
2024-11-16 17:03:12 -0300

    (data_utils) - Adds the function to download data directly from the data lake.

--- data_engineering/data_utils.py
+++ data_engineering/data_utils.py
@@ -1,5 +1,23 @@
 import pandas as pd
 
+from io import StringIO
+from utils.lake_connector import LakeConnector
+
+def download_data() -> pd.DataFrame:
+    """
+    Download Petrobras Time serie from Data Lake. 
+    """
+    dl_connection = LakeConnector('azure')
+    blob_name = 'petro/petro.csv'
+    blob = dl_connection.connect(blob_name)
+    blob_data = blob.download_blob()
+    blob_content = blob_data.readall()
+    csv_data = blob_content.decode('utf-8')
+    data = pd.read_csv(StringIO(csv_data), index_col=0, parse_dates=[0])
+    data.index.name = 'Date'
+    print(f'Download from silver/{blob_name} was sucessful.')
+    return data
+
 
 def add_lags(data: pd.DataFrame, num_lags: int, columns: list) -> pd.DataFrame:
     """
@@ -12,3 +30,10 @@ def add_lags(data: pd.DataFrame, num_lags: int, columns: list) -> pd.DataFrame:
             df[f"{column}_(t-{i})"] = df[column].shift(i)
     df.dropna(inplace=True)
     return df
+
+if __name__ == "__main__":
+    
+    df = download_data()
+    print(df.tail())
+    df = add_lags(df, 7, ['pbr'])
+    print(df.tail())
